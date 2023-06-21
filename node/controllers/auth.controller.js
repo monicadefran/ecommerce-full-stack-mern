ef9763fb808ccb398e5d00d9854dd415b48f4b40
monicadefran <monicadefran@gmail.com>
2023-06-21 14:08:07 +0200

    Keep Token and encrypted  user

--- node/controllers/auth.controller.js
+++ node/controllers/auth.controller.js
@@ -1,4 +1,8 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
+import { createAccessToken } from '../libs/jwt.js'; 
+
 
 
 export const register = async (req, res) => {
@@ -6,21 +10,31 @@ export const register = async (req, res) => {
    const {email, password, username} = req.body;
 
    try {
+      // Encriptamos la contraseña
+      const passwordHash = await bcrypt.hash(password,10);
+
       const newUser = new User ({
          username,
          email, 
-         password });
+         password:passwordHash,
+       });
    
       
       const userSaved = await newUser.save();
-      res.json(userSaved);
+      const token = await createAccessToken ({id: userSaved._id});
+
       
+         res.cookie( 'token', token );
+         res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+         });
    } catch (error) {
-      console.log (error);
+      res.status(500).json({message: error.message});
    }
    
-   
 
-   res.send ('registrando');
+   //res.send ('registrando');
 };
 export const login = async (req, res) => res.send("login");
